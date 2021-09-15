140e36664fb205f63271e0233874385043c09c2c
noyyo <dbswltjr7@gmail.com>
2021-09-15 17:46:03 +0900

    9.15 file

---
+++ 콘솔앱/콘솔앱/콘솔앱.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ifstream fin("testdata.txt");
+	if (!fin)
+		cout << "file loading failed" << endl;
+	int data;
+
+	char c;
+	string str = "";
+	
+	while ((c = fin.get()) != EOF) 
+	{
+		if (c == ' ') {
+			data = stoi(str);
+			str = "";
+			cout << data << endl;
+		}
+		else 
+		{
+			str.append(1, c);
+			cout << str << " test "<< endl;
+		}
+	}
+
+}
